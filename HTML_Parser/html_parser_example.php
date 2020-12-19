4f193b4c0a7694486ab15ee1f342f188af745aa8
Preferred <Preferred@Lohith-PC>
2020-12-19 10:17:58 +0530

    Commit 2- success dump of data

--- HTML_Parser/html_parser_example.php
+++ HTML_Parser/html_parser_example.php
@@ -3,16 +3,33 @@
 //Step1: Get the information from the website.
 $url_to_parse= "https://journals.sagepub.com/editorial-board/TAI";
 
-$curl_init= curl_init();
+$ch= curl_init();
 //opt url to parse.
-curl_setopt($curl_init, CURLOPT_URL, $url_to_parse); 
+curl_setopt($ch, CURLOPT_URL, $url_to_parse); 
 //set curlopt_returntransfer true to save response to a variable.
-curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, TRUE); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
 //execute
 
-$html_to_parse= curl_exec($curl_init);
+$html_to_parse= curl_exec($ch);
 //dump the data.
-var_dump($html_to_parse);
+//var_dump($html_to_parse);
+
+//Get all the elements of h1.
+$dom= new DOMDocument();
+@ $dom->loadHTML($html_to_parse);
+
+$all_h1= $dom->getElementsByTagName('h1');
+$h1_array= array();
+foreach($all_h1 as $h1){
+	$h1_array[]= $h1->textContent;
+	print_r($h1_array);
+}
+
+
 
 
 ?>
--- HTML_Parser/readme.txt
+++ HTML_Parser/readme.txt
@@ -1,3 +1,6 @@
 1. HTML parsing is also called as webscrapping.
 2. Use this to get data from another website.
 3. Modify the HTML output of your own website using this data.
+
+4. Important classes used in this is The DOMDocument class , DOMNode class, 
+	DOMNodeList class, DOMElement class, DOMXPath class.
