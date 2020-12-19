4ad155afc7e5eb8bd3d5f16c19d73ab15f9c02b9
Preferred <Preferred@Lohith-PC>
2020-12-19 09:51:00 +0530

    read the url

--- HTML_Parser/html_parser_example.php
+++ HTML_Parser/html_parser_example.php
@@ -1,1 +1,18 @@
 <?php
+
+//Step1: Get the information from the website.
+$url_to_parse= "https://journals.sagepub.com/editorial-board/TAI";
+
+$curl_init= curl_init();
+//opt url to parse.
+curl_setopt($curl_init, CURLOPT_URL, $url_to_parse); 
+//set curlopt_returntransfer true to save response to a variable.
+curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, TRUE); 
+//execute
+
+$html_to_parse= curl_exec($curl_init);
+//dump the data.
+var_dump($html_to_parse);
+
+
+?>
