9162d3eab234a59ee8b0de0bd4c131415577a1ae
Preferred <Preferred@Lohith-PC>
2020-12-19 10:21:08 +0530

    Commit success dump

--- HTML_Parser/html_parser_example.php
+++ HTML_Parser/html_parser_example.php
@@ -8,11 +8,26 @@ $ch= curl_init();
 curl_setopt($ch, CURLOPT_URL, $url_to_parse); 
 //set curlopt_returntransfer true to save response to a variable.
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
 //execute
 
 $html_to_parse= curl_exec($ch);
 //dump the data.
-var_dump($html_to_parse);
+//var_dump($html_to_parse);
+//Get all the elements of h1.
+$dom= new DOMDocument();
+@ $dom->loadHTML($html_to_parse);
+
+$all_h1= $dom->getElementsByTagName('h1');
+$h1_array= array();
+foreach($all_h1 as $h1){
+	$h1_array[]= $h1->textContent;
+	print_r($h1_array);
+}
+
 
 
 
